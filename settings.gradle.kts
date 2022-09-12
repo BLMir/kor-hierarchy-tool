e33d497a6778ad728b96984cf18f2d2aa0b14d17
Biel Mir <biel.mirmesquida@metro-markets.de>
2022-09-12 11:49:53 +0200

    QuickOff service

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "dev.biel.ktor-hierarchy-tool"
---
+++ src/main/kotlin/dev/biel/plugins/Serialization.kt
@@ -0,0 +1,26 @@
+package dev.biel.plugins
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+        gson {
+            }
+    }
+
+    routing {
+        get("/json/kotlinx-serialization") {
+                call.respond(mapOf("hello" to "world"))
+            }
+        get("/json/gson") {
+                call.respond(mapOf("hello" to "world"))
+            }
+    }
+}
